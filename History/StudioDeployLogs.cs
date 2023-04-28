a71f3265ed6bb788ec91a5cf1451f14c9b5abf97
Hexcede <8394472+Hexcede@users.noreply.github.com>
2023-04-27 20:54:11 -0400

    Remove MakeDistinct & auto detect instead

--- History/StudioDeployLogs.cs
+++ History/StudioDeployLogs.cs
@@ -29,34 +29,6 @@ namespace RobloxDeployHistory
             LogCache[channel] = this;
         }
 
-        private static void MakeDistinct(HashSet<DeployLog> targetSet)
-        {
-            var byChangelist = new Dictionary<int, DeployLog>();
-            var rejected = new List<DeployLog>();
-
-            foreach (DeployLog log in targetSet)
-            {
-                int changelist = log.Changelist;
-
-                if (byChangelist.ContainsKey(changelist))
-                {
-                    DeployLog oldLog = byChangelist[changelist];
-
-                    if (oldLog.TimeStamp.CompareTo(log.TimeStamp) < 0)
-                    {
-                        byChangelist[changelist] = log;
-                        rejected.Add(oldLog);
-                    }
-                }
-                else
-                {
-                    byChangelist.Add(changelist, log);
-                }
-            }
-
-            rejected.ForEach(log => targetSet.Remove(log));
-        }
-
         private async Task<DeployLog> GetLiveLog(Channel channel, string binaryType)
         {
             var info = await ClientVersionInfo.Get(channel, binaryType);
@@ -85,8 +57,53 @@ namespace RobloxDeployHistory
             CurrentLogs_x86.Clear();
             CurrentLogs_x64.Clear();
 
-            if (deployHistory.Contains("version-hidden"))
+            var matches = Regex.Matches(deployHistory, LogPattern);
+
+            foreach (Match match in matches)
             {
+                string[] data = match.Groups.Cast<Group>()
+                    .Select(group => group.Value)
+                    .Where(value => value.Length != 0)
+                    .ToArray();
+
+                string buildType = data[1];
+                string versionGuid = data[2];
+
+                if (versionGuid.ToLowerInvariant() == "version-hidden")
+                    continue;
+
+                var deployLog = new DeployLog()
+                {
+                    VersionGuid = versionGuid,
+                    TimeStamp = DateTime.Parse(data[3], DateTimeFormatInfo.InvariantInfo),
+
+                    MajorRev = int.Parse(data[4], NumberFormat),
+                    Version = int.Parse(data[5], NumberFormat),
+                    Patch = int.Parse(data[6], NumberFormat),
+                    Changelist = int.Parse(data[7], NumberFormat),
+
+                    Is64Bit = buildType.EndsWith("64", StringFormat),
+                    Channel = channel,
+                    GitHash = data[8]
+                };
+
+                // Block builds before 550 due to RCE.
+                if (deployLog.Version < 550)
+                    continue;
+
+                HashSet<DeployLog> targetList;
+
+                if (deployLog.Is64Bit)
+                    targetList = CurrentLogs_x64;
+                else
+                    targetList = CurrentLogs_x86;
+
+                targetList.Add(deployLog);
+            }
+
+            HasHistory = CurrentLogs_x64.Count > 0 || CurrentLogs_x86.Count > 0;
+
+            if (!HasHistory) {
                 if (Environment.Is64BitOperatingSystem)
                 {
                     var liveInfo_x64 = await GetLiveLog(channel, "WindowsStudio64");
@@ -97,59 +114,6 @@ namespace RobloxDeployHistory
                     var liveInfo_x86 = await GetLiveLog(channel, "WindowsStudio");
                     CurrentLogs_x86.Add(liveInfo_x86);
                 }
-
-                HasHistory = false;
-            }
-            else
-            {
-                var matches = Regex.Matches(deployHistory, LogPattern);
-
-                foreach (Match match in matches)
-                {
-                    string[] data = match.Groups.Cast<Group>()
-                        .Select(group => group.Value)
-                        .Where(value => value.Length != 0)
-                        .ToArray();
-
-                    string buildType = data[1];
-                    string versionGuid = data[2];
-
-                    if (versionGuid.ToLowerInvariant() == "version-hidden")
-                        continue;
-
-                    var deployLog = new DeployLog()
-                    {
-                        VersionGuid = versionGuid,
-                        TimeStamp = DateTime.Parse(data[3], DateTimeFormatInfo.InvariantInfo),
-
-                        MajorRev = int.Parse(data[4], NumberFormat),
-                        Version = int.Parse(data[5], NumberFormat),
-                        Patch = int.Parse(data[6], NumberFormat),
-                        Changelist = int.Parse(data[7], NumberFormat),
-
-                        Is64Bit = buildType.EndsWith("64", StringFormat),
-                        Channel = channel,
-                        GitHash = data[8]
-                    };
-
-                    // Block builds before 550 due to RCE.
-                    if (deployLog.Version < 550)
-                        continue;
-
-                    HashSet<DeployLog> targetList;
-
-                    if (deployLog.Is64Bit)
-                        targetList = CurrentLogs_x64;
-                    else
-                        targetList = CurrentLogs_x86;
-
-                    targetList.Add(deployLog);
-                }
-
-                MakeDistinct(CurrentLogs_x64);
-                MakeDistinct(CurrentLogs_x86);
-
-                HasHistory = true;
             }
         }
 
